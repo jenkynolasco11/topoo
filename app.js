78bde95387ac67af9132bb4ad66b8e44bb9a368b
Jenky W. Nolasco <jenky.nolasco@gmail.com>
2016-10-18 12:31:38 -0300

    slightly changes to configuration

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 'use strict';
-var port = process.argv[2],
-	express = require('express'),
-	app = express();
+var port = process.argv[2];
+var express = require('express');
+var app = express();
 
 // Setting static and engine
 app.use('/static',express.static(__dirname + '/static'));
+
+// Setting the views and view engine
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
 // Middlewares to use
-var error_page = require('./middlewares/404'),
-	routes = require('./routes/routes');
+var error_page = require('./middlewares/404');
+var routes = require('./routes/routes');
+var orders = require('./routes/orders');
 
 // Loading middlewares
 app.use('/',routes);
 app.use('/',error_page);
+app.use('/orders', orders);
 
 //Starting Server
 console.log('Starting server at port: ' + (port || 8080) + '...');
